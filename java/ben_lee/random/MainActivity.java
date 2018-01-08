c18d34f1d06f3c8e800e8cc07fcbdafec7ede275
Ben Lee <b.e.n_lee@live.com>
2018-01-08 14:13:40 +1100

    Random number generator with changeable upper and lower bounds (between -9999 and 9999) Navigation drawer with no working options MainActivity uses fragments

---
+++ java/ben_lee/random/MainActivity.java
@@ -0,0 +1,71 @@
+package ben_lee.random;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private String[] choices_array;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar appBar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(appBar);
+        initialiseFragment(savedInstanceState);
+        createDrawer();
+    }
+
+    private void initialiseFragment(Bundle savedInstanceState) {
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            RandomNumberFragment randNumFragment = new RandomNumberFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, randNumFragment).commit();
+        }
+    }
+
+    private void createDrawer() {
+        choices_array = getResources().getStringArray(R.array.choices_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.drawer_list);
+        TextView listHeader = findViewById(R.id.drawer_title);
+        listHeader.setText(getResources().getString(R.string.options));
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, choices_array));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        Toolbar appBar = (Toolbar) findViewById(R.id.app_bar);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, appBar, R.string.drawer_open, R.string.drawer_open) {
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+            }
+
+            public void onDrawerOpen(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        mDrawerToggle.syncState();
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+            mDrawerLayout.closeDrawer(findViewById(R.id.left_drawer));
+        }
+    }
+
+}
---
+++ java/ben_lee/random/RandomNumberFragment.java
@@ -0,0 +1,58 @@
+package ben_lee.random;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.security.SecureRandom;
+
+public class RandomNumberFragment extends Fragment {
+    private static final int DEFAULT_UPPER_BOUND = 1000;
+    private static final int DEFAULT_LOWER_BOUND = 0;
+    private static final int UPPER_BOUND_LIMIT = 9999;
+    private static final int LOWER_BOUND_LIMIT = -9999;
+
+    SecureRandom random = new SecureRandom();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.random_number_fragment, container, false);
+    }
+
+    public void generateNew(View view) {
+        TextView numberView = (TextView) getView().findViewById(R.id.numberView);
+        numberView.setTextSize(60);
+        numberView.setText(String.valueOf(generateInt()));
+    }
+
+    private int generateInt() {
+        EditText lowerBound = (EditText) getView().findViewById(R.id.num_lower_bound);
+        EditText upperBound = (EditText) getView().findViewById(R.id.num_upper_bound);
+        String lowBoundStr = lowerBound.getText().toString();
+        String upBoundStr = upperBound.getText().toString();
+        int lowBound = lowBoundStr.isEmpty() ? DEFAULT_LOWER_BOUND : Integer.parseInt(lowBoundStr);
+        int upBound = upBoundStr.isEmpty() ? DEFAULT_UPPER_BOUND : Integer.parseInt(upBoundStr);
+        if (lowBound > upBound) {
+            int temp = lowBound;
+            lowBound = upBound;
+            upBound = temp;
+        }
+        if (upBound > UPPER_BOUND_LIMIT) {
+            upBound = UPPER_BOUND_LIMIT;
+            upperBound.setText(String.valueOf(upBound));
+        }
+        if (lowBound < LOWER_BOUND_LIMIT) {
+            lowBound = LOWER_BOUND_LIMIT;
+            lowerBound.setText(String.valueOf(lowBound));
+        }
+        int difference = upBound - lowBound;
+        return random.nextInt(difference) + lowBound;
+    }
+}
